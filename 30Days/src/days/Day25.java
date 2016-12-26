2a94e6dacecea28364e1ac27b04fa970bc9c557c
dvenk <dvenk@karthikpc>
2016-12-25 21:40:15 -0500

    Day 25: Check prime in optimal time complexity

---
+++ 30Days/src/days/Day25.java
@@ -0,0 +1,35 @@
+package days;
+
+import java.util.Scanner;
+
+public class Day25 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T-- > 0){
+			int x = sc.nextInt();
+			checkPrime(x);
+		}
+		sc.close();
+	}
+	
+	static void checkPrime(int n){
+		int sq = (int) Math.sqrt(n);
+		
+		if(n == 1){
+			System.out.println("Not prime");
+			return;
+		}
+		
+		for(int i=2; i<=sq; i++){
+			if(n%i == 0){
+				System.out.println("Not prime");
+				return;
+			}
+		}
+		System.out.println("Prime");
+	}
+
+}
